902dafe9ac09c2dbaa6c35f77b76b1e89ad1db8d
Mostafa Mosad <mustafamosad57@gmail.com>
2020-06-14 15:54:23 -0500

    delete Repo for airline and airport

--- crudService/src/main/java/com/cs544/group7/crudService/repository/AirlineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cs544.group7.crudService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cs544.group7.crudService.domain.Airline;
-
-public interface AirlineRepository extends JpaRepository<Airline, Integer> {
-	public Airline findByCode(String airlineCode);
-}
--- crudService/src/main/java/com/cs544/group7/crudService/repository/AirportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cs544.group7.crudService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cs544.group7.crudService.domain.Airport;
-
-public interface AirportRepository extends JpaRepository<Airport, Integer> {
-	public Airport findByCode(String airportCode);
-}
