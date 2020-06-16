0175500a813abf31293b365e40822c41a16c67f4
Mostafa Mosad <mustafamosad57@gmail.com>
2020-06-15 23:25:55 -0500

    adding reservation domain

--- crudService/src/main/java/com/cs544/group7/crudService/domain/Flight.java
+++ crudService/src/main/java/com/cs544/group7/crudService/domain/Flight.java
@@ -7,79 +7,80 @@ import javax.persistence.*;
 @Entity
 public class Flight {
 
-    @Id
-    @GeneratedValue
-    private Integer id;
-    
-    @Column(length = 4, nullable = false)
-    private int flight_number;
-    
-    @Column(nullable = false)
-    private int capacity;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 
-    @ManyToOne
-    @JoinColumn(name = "destinationAirport_id")
-    private Airport destinationAirport;
-    
-    @Temporal(TemporalType.TIME)
-    private Date destinationTime;
-    
-    @Temporal(TemporalType.DATE)
-    private Date destinationDate;
-    
-    @ManyToOne
-    @JoinColumn(name = "arrivalAirport_id")
-    private Airport arrivalAirport;
-    
-    @Temporal(TemporalType.TIME)
-    private Date arrivalTime;
-    
-    @Temporal(TemporalType.DATE)
-    private Date arrivalDate;
-    
-    @ManyToOne
-    @JoinColumn(name = "airlineid")
-    private Airline airline;
-    
-    // default constructor
-    public Flight(){}
-    
-    public Integer getId() {
-        return id;
-    }
+	@Column(length = 4, nullable = false)
+	private int flight_number;
+
+	@Column(nullable = false)
+	private int capacity;
+
+	@ManyToOne
+	@JoinColumn(name = "destinationAirport_id")
+	private Airport destinationAirport;
+
+	@Temporal(TemporalType.TIME)
+	private Date destinationTime;
+
+	@Temporal(TemporalType.DATE)
+	private Date destinationDate;
+
+	@ManyToOne
+	@JoinColumn(name = "arrivalAirport_id")
+	private Airport arrivalAirport;
+
+	@Temporal(TemporalType.TIME)
+	private Date arrivalTime;
+
+	@Temporal(TemporalType.DATE)
+	private Date arrivalDate;
+
+	@ManyToOne
+	@JoinColumn(name = "airlineid")
+	private Airline airline;
+
+	// default constructor
+	public Flight() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
 
 	public void setId(Integer id) {
-        this.id = id;
-    }
+		this.id = id;
+	}
 
-    public int getFlight_number() {
-        return flight_number;
-    }
+	public int getFlight_number() {
+		return flight_number;
+	}
 
-    public void setFlight_number(int flight_number) {
-        this.flight_number = flight_number;
-    }
+	public void setFlight_number(int flight_number) {
+		this.flight_number = flight_number;
+	}
 
-    public int getCapacity() {
-        return capacity;
-    }
+	public int getCapacity() {
+		return capacity;
+	}
 
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
 
-    public Airport getDestinationAirport() {
-        return destinationAirport;
-    }
+	public Airport getDestinationAirport() {
+		return destinationAirport;
+	}
 
-    public void setDestinationAirport(Airport destinationAirport) {
-        this.destinationAirport = destinationAirport;
-    }
-    
-    public Date getDestinationTime() {
+	public void setDestinationAirport(Airport destinationAirport) {
+		this.destinationAirport = destinationAirport;
+	}
+
+	public Date getDestinationTime() {
 		return destinationTime;
 	}
-    
+
 	public Date getDestinationDate() {
 		return destinationDate;
 	}
@@ -87,27 +88,26 @@ public class Flight {
 	public void setDestinationDate(Date destinationDate) {
 		this.destinationDate = destinationDate;
 	}
-	
+
 	public void setDestinationTime(Date destinationTime) {
 		this.destinationTime = destinationTime;
 	}
 
 	public Airport getArrivalAirport() {
-        return arrivalAirport;
-    }
+		return arrivalAirport;
+	}
 
-    public void setArrivalAirport(Airport arrivalAirport) {
-        this.arrivalAirport = arrivalAirport;
-    }
-    
-    public Date getArrivalTime() {
+	public void setArrivalAirport(Airport arrivalAirport) {
+		this.arrivalAirport = arrivalAirport;
+	}
+
+	public Date getArrivalTime() {
 		return arrivalTime;
 	}
 
 	public void setArrivalTime(Date arrivalTime) {
 		this.arrivalTime = arrivalTime;
 	}
-	
 
 	public Date getArrivalDate() {
 		return arrivalDate;
@@ -118,10 +118,10 @@ public class Flight {
 	}
 
 	public Airline getAirline() {
-        return airline;
-    }
+		return airline;
+	}
 
-    public void setAirline(Airline airline) {
-        this.airline = airline;
-    }
+	public void setAirline(Airline airline) {
+		this.airline = airline;
+	}
 }
---
+++ reservationService/src/main/java/com/cs544/group7/reservationService/res/ResponseFlight.java
@@ -0,0 +1,102 @@
+package com.cs544.group7.reservationService.res;
+
+import java.util.Date;
+
+public class ResponseFlight {
+
+	private int flight_number;
+
+	private String destinationAirport;
+
+	private Date destinationTime;
+
+	private Date destinationDate;
+
+	private String arrivalAirport;
+
+	private Date arrivalTime;
+
+	private Date arrivalDate;
+
+	private String airlineName;
+
+	public ResponseFlight() {
+	}
+
+	public ResponseFlight(int flight_number, String destinationAirport, Date destinationTime, Date destinationDate,
+			String arrivalAirport, Date arrivalTime, Date arrivalDate, String airlineName) {
+		this.flight_number = flight_number;
+		this.destinationAirport = destinationAirport;
+		this.destinationTime = destinationTime;
+		this.destinationDate = destinationDate;
+		this.arrivalAirport = arrivalAirport;
+		this.arrivalTime = arrivalTime;
+		this.arrivalDate = arrivalDate;
+		this.airlineName = airlineName;
+	}
+
+	public int getFlight_number() {
+		return flight_number;
+	}
+
+	public void setFlight_number(int flight_number) {
+		this.flight_number = flight_number;
+	}
+
+	public String getDestinationAirport() {
+		return destinationAirport;
+	}
+
+	public void setDestinationAirport(String destinationAirport) {
+		this.destinationAirport = destinationAirport;
+	}
+
+	public Date getDestinationTime() {
+		return destinationTime;
+	}
+
+	public void setDestinationTime(Date destinationTime) {
+		this.destinationTime = destinationTime;
+	}
+
+	public Date getDestinationDate() {
+		return destinationDate;
+	}
+
+	public void setDestinationDate(Date destinationDate) {
+		this.destinationDate = destinationDate;
+	}
+
+	public String getArrivalAirport() {
+		return arrivalAirport;
+	}
+
+	public void setArrivalAirport(String arrivalAirport) {
+		this.arrivalAirport = arrivalAirport;
+	}
+
+	public Date getArrivalTime() {
+		return arrivalTime;
+	}
+
+	public void setArrivalTime(Date arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+
+	public Date getArrivalDate() {
+		return arrivalDate;
+	}
+
+	public void setArrivalDate(Date arrivalDate) {
+		this.arrivalDate = arrivalDate;
+	}
+
+	public String getAirlineName() {
+		return airlineName;
+	}
+
+	public void setAirlineName(String airlineName) {
+		this.airlineName = airlineName;
+	}
+
+}
