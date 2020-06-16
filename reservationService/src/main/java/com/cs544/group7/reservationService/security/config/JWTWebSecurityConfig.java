0ef10b35b8874390c33f1b6a1e84b9609c7542b6
Mostafa Mosad <mustafamosad57@gmail.com>
2020-06-16 00:39:28 -0500

    adding security to reservation service

---
+++ reservationService/src/main/java/com/cs544/group7/reservationService/security/config/JWTWebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.cs544.group7.reservationService.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.cs544.group7.reservationService.security.filter.JwtTokenAuthorizationOncePerRequestFilter;
+import com.cs544.group7.reservationService.security.filter.JwtUnAuthorizedResponseAuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class JWTWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private JwtUnAuthorizedResponseAuthenticationEntryPoint jwtUnAuthorizedResponseAuthenticationEntryPoint;
+
+	@Autowired
+	private JwtTokenAuthorizationOncePerRequestFilter jwtAuthenticationTokenFilter;
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable().exceptionHandling()
+				.authenticationEntryPoint(jwtUnAuthorizedResponseAuthenticationEntryPoint).and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				.antMatchers("/v2/api-docs", "/configuration/ui", "/resources/**", "/swagger-resources/**",
+						"/configuration/security", "/swagger-ui.html", "/webjars/**")
+				.permitAll().anyRequest().authenticated().and()
+				.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+	}
+
+}
