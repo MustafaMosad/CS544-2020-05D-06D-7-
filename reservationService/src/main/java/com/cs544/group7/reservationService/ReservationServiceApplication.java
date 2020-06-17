c3573283ef2319900361efd21bee0753467b2a7e
paudelnavin <navinpaudel@outlook.com>
2020-06-17 18:17:35 -0500

    some changes

--- reservationService/src/main/java/com/cs544/group7/reservationService/ReservationServiceApplication.java
+++ reservationService/src/main/java/com/cs544/group7/reservationService/ReservationServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.cs544.group7.reservationService.producer.MessageSenderConfirm;
 import com.cs544.group7.reservationService.producer.MessageSenderSchedule;
@@ -14,6 +15,7 @@ import com.cs544.group7.reservationService.res.ReservationResponse;
 @SpringBootApplication
 @EnableEurekaClient
 @ComponentScan(basePackages = "com.cs544.group7.reservationService")
+@EnableScheduling
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
--- reservationService/src/main/java/com/cs544/group7/reservationService/producer/MessageSenderSchedule.java
+++ reservationService/src/main/java/com/cs544/group7/reservationService/producer/MessageSenderSchedule.java
@@ -1,13 +1,15 @@
 package com.cs544.group7.reservationService.producer;
 
+import org.apache.activemq.ScheduledMessage;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.cs544.group7.emailService.dto.EmailDto;
 
+//@EnableScheduling
 public class MessageSenderSchedule {
 		
-	@Scheduled(cron="* 28 16 * * *")
+//	@Scheduled(cron="0 30 18 * * *")
 	public static void scheduleAndSend(JmsTemplate jmsTemplate) {
 		
 		EmailDto emailDto = new EmailDto();
@@ -17,6 +19,9 @@ public class MessageSenderSchedule {
 		
 		System.out.println(">> 2 >> Sent an schedule email message.");
 	    jmsTemplate.convertAndSend("message_queue", new EmailDto(emailAddress, "Schedule", 
-	    		                   "Dear Customer, your flight is tomorrow").toString());
+	    		                   "Dear Customer, your flight is tomorrow").toString(),  m -> {
+	    		                       m.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_DELAY, 300000);
+	    		                       	return m;
+	    		                   });
 	}
 }
