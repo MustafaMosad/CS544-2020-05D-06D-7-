f1b0db9d8fbc2002907fc18ffdbd312822a9d809
Mostafa Mosad <mustafamosad57@gmail.com>
2020-06-17 14:47:51 -0500

    Adding generating reservation code

--- reservationService/src/main/java/com/cs544/group7/reservationService/service/ReservationServiceImpl.java
+++ reservationService/src/main/java/com/cs544/group7/reservationService/service/ReservationServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,57 +21,57 @@ import com.cs544.group7.reservationService.util.CrudServiceCaller;
 @Service
 @Transactional
 public class ReservationServiceImpl implements ReservationService {
-	
 
 	@Autowired
 	ReservationRepository reservationRepository;
-	
+
 	@Autowired
 	CrudServiceCaller crudServiceCaller;
-	
+
 	@Override
 	public List<ResponseReservation> getAllReservations() {
-		
-		return reservationRepository.findAll()
-				.stream()
-				.parallel()
-				.map(this::convertReservationToReservationResponse)
+
+		return reservationRepository.findAll().stream().parallel().map(this::convertReservationToReservationResponse)
 				.collect(Collectors.toList());
 	}
-	
+
 	private ResponseReservation convertReservationToReservationResponse(Reservation reservation) {
-		return new ResponseReservation(reservation.getReservationCode(), reservedFlights(reservation.getFlightNumbers()), 
-										reservation.isConfirmed(), reservation.getCreatedAt(), 
-										getPasssengerFirstName(reservation.getPassengerId()),
-										getPasssengerFirstName(reservation.getPassengerId())
-									);
+		return new ResponseReservation(reservation.getReservationCode(),
+				reservedFlights(reservation.getFlightNumbers()), reservation.isConfirmed(), reservation.getCreatedAt(),
+				getPasssengerFirstName(reservation.getPassengerId()),
+				getPasssengerFirstName(reservation.getPassengerId()));
 	}
-	
-	private List<ResponseFlight> reservedFlights (Set<Integer> reservedFlightNumbers){
+
+	private List<ResponseFlight> reservedFlights(Set<Integer> reservedFlightNumbers) {
 		return new ArrayList<ResponseFlight>();
 	}
-	
+
 	private String getPasssengerFirstName(Long passengerId) {
 		return passengerId.toString();
 	}
 
 	@Override
 	public void addNewReservation(RequestReservation requestReservation) {
-		
+
 		reservationRepository.save(convertRequestReservationToReservation(requestReservation));
-		
+
 	}
-	
+
 	private Reservation convertRequestReservationToReservation(RequestReservation requestReservation) {
 		Reservation reservation = new Reservation();
+		reservation.setReservationCode(generateReservationCode());
 		reservation.setPassengerId(requestReservation.getPassengerId());
 		reservation.setMadeByAgentId(requestReservation.getAgentId());
 		reservation.setMadeByUserId(requestReservation.getPassengerId());
 		reservation.setFlightNumbers(requestReservation.getFlightNumbers());
 		reservation.setCreatedAt(new Date());
 		return reservation;
-		
-		
+
+	}
+
+	private String generateReservationCode() {
+		// TODO Auto-generated method stub
+		return UUID.randomUUID().toString();
 	}
 
 	@Override
@@ -81,18 +82,15 @@ public class ReservationServiceImpl implements ReservationService {
 
 	@Override
 	public List<ResponseReservation> getPassengerReservations(Long passengerId) {
-		return reservationRepository.findByPassengerId(passengerId)
-				.stream()
-				.parallel()
-				.map(this::convertReservationToReservationResponse)
-				.collect(Collectors.toList());
+		return reservationRepository.findByPassengerId(passengerId).stream().parallel()
+				.map(this::convertReservationToReservationResponse).collect(Collectors.toList());
 	}
 
 	@Override
 	public void cancelReservation(String reservationCode) {
 		reservationRepository.delete(reservationRepository.findByReservationCode(reservationCode));
 	}
-	
+
 	@Override
 	public ResponseFlight getFlight(Integer flightNumber) {
 		System.out.println("Also I am here");
